a5d0ea7ae7c1a0bbc12a826b8d84f909b5fccb6e
Maulidan Nashuha <maulidannashuha@gmail.com>
2022-03-25 23:53:09 +0700

    [Maulidan] refactor to game class

--- mini-game-oop.js
+++ mini-game-oop.js
@@ -1,3 +1,37 @@
+class Game{
+    constructor(){
+        this.player = new Player("Player", "player-number")
+        this.computer = new Player("Computer", "computer-number")
+    }
+
+    run(){
+        this.player.randomScore()    
+        this.computer.randomScore()
+
+        let winner = this.#getWinner()
+        this.#showWinner(winner)
+    }
+
+    #getWinner(){
+        if(this.player.score > this.computer.score){
+            // player menang
+            return "Pemenangnya adalah Player"
+        }else if(this.player.score < this.computer.score){
+            // computer menang
+            return "Pemenangnya adalah Computer"
+        }else{
+            // seri
+            return "Permainan seri"
+        }
+    }
+
+    // simbol # menandakan method random di set menjadi private
+    #showWinner(text){
+        let winner = document.getElementById("winner")
+        winner.textContent = text
+    }
+}
+
 class Player{
     constructor(name, elementId){
         this.name = name
@@ -13,33 +47,13 @@ class Player{
         this.score = score
     }
 
-    // tanda # menandakan function random di set menjadi private
+    // simbol # menandakan method random di set menjadi private
     #random(){
         return Math.floor(Math.random() * 100 + 1)
     }
 }
 
 function start(){
-    let player = new Player("Player", "player-number")
-    player.randomScore()
-
-    let computer = new Player("Computer", "computer-number")
-    computer.randomScore()
-
-
-    if(player.score > computer.score){
-        // player menang
-        setWinner("Pemenangnya adalah Player")
-    }else if(player.score < computer.score){
-        // computer menang
-        setWinner("Pemenangnya adalah Computer")
-    }else{
-        // seri
-        setWinner("Permainan seri")
-    }
-}
-
-function setWinner(text){
-    let winner = document.getElementById("winner")
-    winner.textContent = text
+    let game = new Game()
+    game.run()    
 }
